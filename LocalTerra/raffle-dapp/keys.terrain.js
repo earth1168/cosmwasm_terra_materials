85fd5c3457e45852c328154c1a0f2058c871bd53
Earth <pimanus1234@gmail.com>
2022-05-03 23:19:36 +0700

    Run frontend on testnet

--- LocalTerra/raffle-dapp/keys.terrain.js
+++ LocalTerra/raffle-dapp/keys.terrain.js
@@ -4,9 +4,6 @@
 module.exports = {
   custom_tester_1: {
     mnemonic:
-      "shiver position copy catalog upset verify cheap library enjoy extend second peasant basic kit polar business document shrug pass chuckle lottery blind ecology stand",
-  },
-  custom_tester_2: {
-    privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
-  },
+      "smile better scheme boat tiger resource alley odor stove lumber win motor manual call elder stamp own uphold off enemy robust law unknown office",
+  }
 };
